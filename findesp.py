216977f9614f3b4c02916c4f0c280e36bf85d718
salmon <947270280@qq.com>
2017-09-17 21:52:40 +0800

    Create findesp.py

---
+++ findesp.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def find_jmpesp(binary):
+    from elftools.elf.elffile import ELFFile
+    data = open(binary, 'rb').read()
+    jmpesp = "\xff\xe4" 
+    if jmpesp in data:
+        index = data.index(jmpesp)
+
+    lastsec = None
+    for sec in ELFFile(open(binary, 'rb')).iter_sections():
+        if index > sec.header.sh_offset:
+            lastsec = sec
+        else:
+            addr = lastsec.header.sh_addr + index - lastsec.header.sh_offset
+            print "addr:", hex(addr)
+            return addr
+    return None
+
+
+
+
+if __name__ == '__main__':
+    addr = find_jmpesp(sys.argv[1])
